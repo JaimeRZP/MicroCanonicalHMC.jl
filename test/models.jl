ff2e1d22b5c66460ab3e7ff62fbe6ee6821fe6e9
jaimerzp <jaimerz011235813@gmail.com>
2024-02-22 14:32:46 +0000

    transformations working

--- test/models.jl
+++ test/models.jl
@@ -6,23 +6,23 @@
         T = Float32
         d=2
         a=T(1.0)
-        b=T(1.0)
+        b=T(10.0)
         target = RosenbrockTarget(a, b, d)
         spl = MCHMC(10_000, 0.01;
         L = sqrt(2), sigma = ones(target.d),
         tune_L = false, tune_sigma = false, adaptive = true)
-        samples = Sample(spl, target, 200_000)
+        samples = Sample(spl, target, 200_000; dialog = true)
         d1 = samples[1, :]
         d2 = samples[2, :]
-        mm1, m1, s1, = (median(d1), mean(d1), var(d1))
-        mm2, m2, s2, = (median(d2), mean(d2), var(d2))
+        mm1, m1, s1, = (median(d1), mean(d1), std(d1))
+        mm2, m2, s2, = (median(d2), mean(d2), std(d2))
         @test eltype(samples[1]) == T
         @test mm1 ≈ 1.00 atol = 0.2
         @test m1 ≈ 1.00 atol = 0.2
         @test s1 ≈ 1.00 atol = 0.3
-        @test mm2 ≈ 1.40 atol = 0.2
+        @test mm2 ≈ 1.13 atol = 0.2
         @test m2 ≈ 1.97 atol = 0.2
-        @test s2 ≈ 6.70 atol = 0.5
+        @test s2 ≈ 2.40 atol = 0.5
     end
 
     @testset "Rosembrok Transformed" begin
@@ -33,9 +33,9 @@
         d=2
         a=T(1.0)
         b=T(1.0)
-        Λ=T(10.0)
-        inv_transform(θ) = Λ .* θ
-        transform(x) = x ./ Λ
+        Λ=T(3.0)
+        transform(θ) = Λ .* θ
+        inv_transform(x) = x ./ Λ
         target = RosenbrockTarget(a, b, d;
             transform = transform,
             inv_transform = inv_transform)
@@ -47,17 +47,18 @@
         θ2 = samples[2, :]
         x1 = samples[3, :]
         x2 = samples[4, :]
-        mθ1, sθ1, = (mean(θ1), var(θ1))
-        mθ2, sθ2, = (mean(θ2), var(θ2))
-        @test mθ1 ≈ 1.00 atol = 0.2
-        @test sθ1 ≈ 1.00 atol = 0.3
-        @test mθ2 ≈ 1.97 atol = 0.2
-        @test sθ2 ≈ 6.70 atol = 0.5
+
+        xx1 = transform(θ1)
+        xx2 = transform(θ2)
+
         mx1, sx1, = (mean(x1), var(x1))
         mx2, sx2, = (mean(x2), var(x2))
-        @test mx1 ≈ transform(1.00) atol = transform(0.2)
-        @test sx1 ≈ transform(1.00) atol = transform(0.3)
-        @test mx2 ≈ transform(1.97) atol = transform(0.2)
-        @test sx2 ≈ transform(6.70) atol = transform(0.5)
+        mxx1, sxx1, = (mean(xx1), var(xx1))
+        mxx2, sxx2, = (mean(xx2), var(xx2))
+
+        @test mx1 ≈ mxx1 atol = transform(0.1)
+        @test sx1 ≈ sxx1 atol = transform(0.1)
+        @test mx2 ≈ mxx2 atol = transform(0.1)
+        @test sx2 ≈ sxx2 atol = transform(0.1)
     end
 end
